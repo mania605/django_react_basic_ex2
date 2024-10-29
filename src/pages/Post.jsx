8c91d3535fbeb35730d28e3aa25f67e4214f6e63
mania605 <kim.minsun83@gmail.com>
2024-10-29 13:02:32 +0900

    짬뽕

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,23 +1,25 @@
-import { useState } from 'react';
 import Layout from '../components/Layout';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Post() {
 	const [Posts, setPosts] = useState([]);
-
 	useEffect(() => {
-		axios.get('http://localhost:8000:posts').then(res => {
+		axios.get('http://localhost:8000/posts').then(res => {
 			console.log(res.data);
 			setPosts(res.data);
 		});
 	}, []);
-
 	return (
 		<Layout title='Post'>
-			{Posts.map((post)=>{
-				return <h3 key={post.id}>
-					<Link to={post.title}></h3>
-			});}
+			{Posts.map(post => {
+				return (
+					<h3 key={post.id}>
+						<Link to={`/detail/${post.slug}`}>{post.title}</Link>
+					</h3>
+				);
+			})}
 		</Layout>
 	);
 }
